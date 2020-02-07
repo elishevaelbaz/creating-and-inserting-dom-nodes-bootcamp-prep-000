ca9916cc4b06b9d39b11f4e7040f9a75fe597e6b
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-07 18:27:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+var element2 = document.createElement('div');
+
+element2.innerHTML = 'Hello, DOM!';
+element2.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(element2);
